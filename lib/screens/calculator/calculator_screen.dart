4d5404137d76f7d70dd2a12d51c580957243cb6d
arifin.zn.dev <arifin.zn.dev@gmail.com>
2023-10-25 16:34:01 +0700

    kalkulator

--- lib/screens/calculator/calculator_screen.dart
+++ lib/screens/calculator/calculator_screen.dart
@@ -75,146 +75,144 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black54,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.black54,
-          leading: const Icon(Icons.settings, color: Colors.orange),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.only(top: 18.0),
-              child: Text('DEG', style: TextStyle(color: Colors.white38)),
-            ),
-            SizedBox(width: 20),
-          ],
+      // backgroundColor: Colors.black54,
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Kalkulator'),
         ),
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Align(
-                alignment: Alignment.bottomRight,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Text(result,
-                                  textAlign: TextAlign.left,
-                                  style: const TextStyle(
-                                      color: Colors.white, fontSize: 80))),
-                          const Icon(Icons.more_vert,
-                              color: Colors.orange, size: 30),
-                          const SizedBox(width: 20),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(equation,
+      ),
+      body:
+          // SafeArea(  child:
+          Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(result,
+                                textAlign: TextAlign.left,
                                 style: const TextStyle(
-                                  fontSize: 40,
-                                  color: Colors.white38,
-                                )),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.backspace_outlined,
-                                color: Colors.orange, size: 30),
-                            onPressed: () {
-                              buttonPressed("⌫");
-                            },
-                          ),
-                          const SizedBox(width: 20),
-                        ],
-                      )
-                    ],
-                  ),
+                                    color: Colors.black, fontSize: 60))),
+                        // const Icon(Icons.more_vert,
+                        //     color: Colors.orange, size: 30),
+                        const SizedBox(width: 10),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Text(equation,
+                              style: const TextStyle(
+                                fontSize: 40,
+                                color: Colors.black38,
+                              )),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.backspace_outlined,
+                              color: Colors.orange, size: 30),
+                          onPressed: () {
+                            buttonPressed("⌫");
+                          },
+                        ),
+                        const SizedBox(width: 20),
+                      ],
+                    )
+                  ],
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  calcButton('AC', Colors.white10, () => buttonPressed('AC')),
-                  calcButton('%', Colors.white10, () => buttonPressed('%')),
-                  calcButton('÷', Colors.white10, () => buttonPressed('÷')),
-                  calcButton("×", Colors.white10, () => buttonPressed('×')),
-                ],
-              ),
-              const SizedBox(height: 10),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calcButton('AC', Colors.white10, () => buttonPressed('AC')),
+                calcButton('%', Colors.white10, () => buttonPressed('%')),
+                calcButton('÷', Colors.white10, () => buttonPressed('÷')),
+                calcButton("×", Colors.white10, () => buttonPressed('×')),
+              ],
+            ),
+            const SizedBox(height: 10),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  calcButton('7', Colors.white24, () => buttonPressed('7')),
-                  calcButton('8', Colors.white24, () => buttonPressed('8')),
-                  calcButton('9', Colors.white24, () => buttonPressed('9')),
-                  calcButton('-', Colors.white10, () => buttonPressed('-')),
-                ],
-              ),
-              const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calcButton('7', Colors.white24, () => buttonPressed('7')),
+                calcButton('8', Colors.white24, () => buttonPressed('8')),
+                calcButton('9', Colors.white24, () => buttonPressed('9')),
+                calcButton('-', Colors.white10, () => buttonPressed('-')),
+              ],
+            ),
+            const SizedBox(height: 10),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  calcButton('4', Colors.white24, () => buttonPressed('4')),
-                  calcButton('5', Colors.white24, () => buttonPressed('5')),
-                  calcButton('6', Colors.white24, () => buttonPressed('6')),
-                  calcButton('+', Colors.white10, () => buttonPressed('+')),
-                ],
-              ),
-              const SizedBox(height: 10),
-              // calculator number buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calcButton('4', Colors.white24, () => buttonPressed('4')),
+                calcButton('5', Colors.white24, () => buttonPressed('5')),
+                calcButton('6', Colors.white24, () => buttonPressed('6')),
+                calcButton('+', Colors.white10, () => buttonPressed('+')),
+              ],
+            ),
+            const SizedBox(height: 10),
+            // calculator number buttons
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
 //mainAxisAlignment: MainAxisAlignment.spaceAround
-                    children: [
-                      Row(
-                        children: [
-                          calcButton(
-                              '1', Colors.white24, () => buttonPressed('1')),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.04),
-                          calcButton(
-                              '2', Colors.white24, () => buttonPressed('2')),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.04),
-                          calcButton(
-                              '3', Colors.white24, () => buttonPressed('3')),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          calcButton('+/-', Colors.white24,
-                              () => buttonPressed('+/-')),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.04),
-                          calcButton(
-                              '0', Colors.white24, () => buttonPressed('0')),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.04),
-                          calcButton(
-                              '.', Colors.white24, () => buttonPressed('.')),
-                        ],
-                      ),
-                    ],
-                  ),
-                  calcButton('=', Colors.orange, () => buttonPressed('=')),
-                ],
-              )
-            ],
-          ),
-        ));
+                  children: [
+                    Row(
+                      children: [
+                        calcButton(
+                            '1', Colors.white24, () => buttonPressed('1')),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.04),
+                        calcButton(
+                            '2', Colors.white24, () => buttonPressed('2')),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.04),
+                        calcButton(
+                            '3', Colors.white24, () => buttonPressed('3')),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        calcButton(
+                            '+/-', Colors.white24, () => buttonPressed('+/-')),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.04),
+                        calcButton(
+                            '0', Colors.white24, () => buttonPressed('0')),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.04),
+                        calcButton(
+                            '.', Colors.white24, () => buttonPressed('.')),
+                      ],
+                    ),
+                  ],
+                ),
+                calcButton('=', Colors.orange, () => buttonPressed('=')),
+              ],
+            )
+          ],
+        ),
+      ),
+      // )
+    );
   }
 }
